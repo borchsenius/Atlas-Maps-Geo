f1dbeec9ddd9f61f5deab8a175e08d3a2e31602a
Hangsolow <Hangsolow@server>
2013-02-01 13:17:51 +0100

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtlasMapsGeo::Application.config.secret_token = 'ef1998a5ea77f4ab28ca759b18cf7b06408348dd7492e2e2c3721faa156374c0eb27e49a484475d9d9642274bdf1b3557cad9fcd941476a823de9f5121a74f75'
