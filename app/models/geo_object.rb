d2decc6f6134498a72e9b3de02ca4dc8f4dc14f5
Hangsolow <Hangsolow@server>
2013-02-04 10:49:55 +0100

    converted to ActiceFedora model

--- app/models/geo_object.rb
+++ app/models/geo_object.rb
@@ -1,3 +1,13 @@
 class GeoObject < ActiveFedora::Base
-  attr_accessible :description, :geo_lat, :geo_lng, :path_to_cop, :path_to_image, :publish_year, :title
+  has_metadata :name => 'descMetadata', :type => ActiveFedora::SimpleDatastream do |m|
+    m.field "description", :string
+    m.field "path_to_cop",:string
+    m.field "path_to_image", :string
+    m.field "publish_year", :date
+    m.field "title", :string
+    m.field "geo_lat", :string
+    m.field "geo_lng", :string
+  end
+
+  delegate_to 'descMetadata', [:description, :geo_lat, :geo_lng, :path_to_cop, :path_to_image, :publish_year, :title]
 end
